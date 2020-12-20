3fed93fb8c474abf5396a50c3c6aff7a5586c604
Connor O'Brien <connor@hifranc.com>
2020-12-20 14:23:02 +0200

    Add script file

---
+++ script.js
@@ -0,0 +1,16 @@
+var firstName = "Mari";
+
+// property
+var numCharacters = firstName.length;
+
+console.log(numCharacters);
+
+// method
+var lower = firstName.toLowerCase();
+
+console.log(lower);
+
+// method
+var upper = firstName.toUpperCase();
+
+console.log(upper);
